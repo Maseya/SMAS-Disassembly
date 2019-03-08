331e12c5ebf919141698faff71e13da9442ead91
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-03-08 00:18:58 -0800

    Move b06.asm to b06\

---
+++ Assembly/b06/_b06.asm
@@ -0,0 +1,11 @@
+; Super Mario All-Stars   (U) disassembly
+; Disassembled by Roy and Ersanio and imamelia and wiiqwertyuiop
+
+DATA_068000:
+    incbin "DATA_068000-2000.bin"
+
+DATA_06A000:
+    incbin "DATA_06A000-2000.bin"
+
+DATA_06C000:
+    incbin "DATA_06C000-4000.bin"
